0d7451f19d4b5d2d0b659efeb9fdb6d9d46d480d
Monstelove <ope_love_606@hotmail.com>
2018-11-01 09:09:21 +0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# testerOpe" 
+"# testerOpe" 
---
+++ sss.bat
@@ -0,0 +1,10 @@
+echo "# testerOpe" >> README.md
+git init
+git add README.md
+pause
+git add *
+pause
+git remote add origin
+pause
+https://github.com/Monsterope/testerOpe.git
+git push -u origin master
